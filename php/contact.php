add3a0b5c39a639b2b0bf69ff0aecf4d09143567
aks96 <31571486+aks96@users.noreply.github.com>
2019-02-13 12:30:37 +0530

    Add files via upload

--- php/contact.php
+++ php/contact.php
@@ -1,36 +1,3 @@
-<!-- <?php
-/*
-THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
-*/
-
-// require 'PHPMailer-master/PHPMailerAutoload.php';
-
-// /*
-// *  CONFIGURE EVERYTHING HERE
-// */
-
-// // an email address that will be in the From field of the email.
-// $fromEmail = 'demo@domain.com';
-// $fromName = 'Demo contact form';
-
-// // an email address that will receive the email with the output of the form
-// $sendToEmail = 'demo@domain.com';
-// $sendToName = 'Demo contact form';
-
-// // subject of the email
-// $subject = 'New message from contact form';
-
-// // form field names and their translations.
-// // array variable name => Text to appear in the email
-// $fields = array('name' => 'Name', 'phone' => 'Phone', 'email' => 'Email', 'need' => 'Need', 'message' => 'Message');
-
-// // message that will be displayed when everything is OK :)
-// $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
-
-// // If something goes wrong, we will display this message.
-// $errorMessage = 'There was an error while submitting the form. Please try again later'; -->
-
-
 <?php
 
 if($_POST["submit"]) {
@@ -43,7 +10,13 @@ if($_POST["submit"]) {
 
     $mailBody="Name: $name\nEmail: $email\nPhone: $phone\n\n$message";
 
-    mail($recipient, $subject, $mailBody, "From: $name <$email>");
+    echo("Recipient : ".$recipient." subject : ".$subject." mail : ".$mailBody);
+
+    $status = mail($recipient, $subject, $mailBody, "From: $name <$email>");
 
     $thankYou="<p>Thank you! Your message has been sent.</p>";
+    echo("<br>".$thankYou);
+    echo("Status : ".$status);
 }
+
+?>
