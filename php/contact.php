64495e70ae3f8da0f2e9d185664ba66e7e71e70a
NAVNEETSINGH1 <navneet.singh@careerpower.in>
2019-02-16 05:34:50 +0530

    My site ready for deployment 3

--- php/contact.php
+++ php/contact.php
@@ -1,22 +1,20 @@
-<?php
+<?php 
+if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-if($_POST["submit"]) {
-    $recipient="sales@windberrysolar.com";
-    $subject="Enquiry Message";
-    $sender=$_POST["name"];
-    $senderEmail=$_POST["email"];
-    $senderPhone=$_POST["phone"];
-    $message=$_POST["message"];
-
-    $mailBody="Name: $name\nEmail: $email\nPhone: $phone\n\n$message";
-
-    echo("Recipient : ".$recipient." subject : ".$subject." mail : ".$mailBody);
-
-    $status = mail($recipient, $subject, $mailBody, "From: $name <$email>");
-
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-    echo("<br>".$thankYou);
-    echo("Status : ".$status);
-}
-
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
 ?>
