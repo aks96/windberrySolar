af63d396f45b83909bb2638c908901fa102901cc
NAVNEETSINGH1 <navneet.singh@careerpower.in>
2019-02-16 07:57:33 +0530

    api testing

--- php/contact.php
+++ php/contact.php
@@ -28,17 +28,24 @@ if(isset($_POST['submit'])){
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
 $from = new SendGrid\Email(null, "sales@windberrysolar.com");
+echo $from;
+
 $subject = "Hello World from the SendGrid PHP Library!";
+echo $subject
 $to = new SendGrid\Email(null, $_POST['email']);
+echo $to
 $content = new SendGrid\Content("text/plain", "Hello, Email!");
+echo $content
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
+echo $mail
 
 $apiKey = getenv('SENDGRID_API_KEY');
+$apiKey
 $sg = new \SendGrid($apiKey);
-
-//$response = $sg->client->mail()->send()->post($mail);
-//echo $response->statusCode();
-//echo $response->headers();
-//echo $response->body();
+$sg 
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
     }
 ?>
