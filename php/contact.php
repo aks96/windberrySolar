01907ae479e844e09398d742bd80117bb6988b18
NAVNEETSINGH1 <navneet.singh@careerpower.in>
2019-02-16 08:00:09 +0530

    api testing

--- php/contact.php
+++ php/contact.php
@@ -27,22 +27,16 @@ if(isset($_POST['submit'])){
 // If you are not using Composer (recommended)
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
-$from = new SendGrid\Email(null, "sales@windberrysolar.com");
-echo $from;
+echo $from = new SendGrid\Email(null, "sales@windberrysolar.com");
 
-$subject = "Hello World from the SendGrid PHP Library!";
-echo $subject
-$to = new SendGrid\Email(null, $_POST['email']);
-echo $to
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
-echo $content
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-echo $mail
+echo $subject = "Hello World from the SendGrid PHP Library!";
+echo $to = new SendGrid\Email(null, $_POST['email']);
+echo $content = new SendGrid\Content("text/plain", "Hello, Email!");
+echo $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$apiKey = getenv('SENDGRID_API_KEY');
-$apiKey
-$sg = new \SendGrid($apiKey);
-$sg 
+echo $apiKey = getenv('SENDGRID_API_KEY');
+echo $sg = new \SendGrid($apiKey);
+
 $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 echo $response->headers();
