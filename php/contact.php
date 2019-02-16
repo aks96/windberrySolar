2c48c39a8694afff8c837197f33d333afbf341dd
NAVNEETSINGH1 <navneet.singh@careerpower.in>
2019-02-16 07:51:06 +0530

    api testing

--- php/contact.php
+++ php/contact.php
@@ -36,9 +36,9 @@ $mail = new SendGrid\Mail($from, $subject, $to, $content);
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+//$response = $sg->client->mail()->send()->post($mail);
+//echo $response->statusCode();
+//echo $response->headers();
+//echo $response->body();
     }
 ?>
